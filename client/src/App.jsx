c2a08d5d660b0f705c5aca966fa15f3caff3b61e
aramirez43 <aramireznicola@cnm.edu>
2022-03-02 00:00:37 -0700

    almost done with team list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div className="app_wrapper">
+    <div className="app__wrapper">
       <Chat client={client} theme="team dark">
         <ChannelListContainer />
         <ChannelContainer />
